6567489f6f690896f0e02f1b9cbf2ecba71f2f1e
Bogdankozlovskiy <bkozlovskiy@bk.ru>
2019-12-20 00:20:56 +0300

    add model

--- src/video/admin.py
+++ src/video/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import MyVideo, Comment
 
-# Register your models here.
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+    extra = 2
+    readonly_fields = ['likes']
+
+
+class MyVideoAdmin(admin.ModelAdmin):
+    inlines = [CommentInline]
+    list_display = ['title', 'date', 'likes', 'player']
+    #exclude = ['likes']
+    readonly_fields = ['likes']
+    prepopulated_fields = {"slug": ["title"]}
+
+
+admin.site.register(MyVideo, MyVideoAdmin)
--- src/video/models.py
+++ src/video/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.safestring import mark_safe
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class MyVideo(models.Model):
+    title = models.CharField(max_length=50, default="NameLess", verbose_name="название")
+    slug = models.SlugField(max_length=50, unique=True, help_text="вводи буквы руками")
+    description = models.TextField(null=True, blank=True)
+    likes = models.PositiveIntegerField(default=0, verbose_name="нравится")
+    url = models.URLField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.slug[:10]}"
+
+    @property
+    def player(self):
+        return mark_safe(f"<iframe width='100' height='100' src='{self.url}' ></iframe>")
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0)
+    video = models.ForeignKey(MyVideo, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
