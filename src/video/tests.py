276270d8d8135586546139b7b78622b5e4403b47
Rozenberg-jv <avangard.npaper@gmail.com>
2019-12-28 13:00:01 +0300

    add test

--- src/video/tests.py
+++ src/video/tests.py
@@ -1,3 +1,81 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import MyVideo, Comment
+from django.contrib.auth.models import User
+from datetime import date
+import logging
+
+
+class VideoTestCase(TestCase):
+	def setUp(self):
+		self.client = Client()
+		video = MyVideo.objects.create(
+			id=1,
+			title="test title",
+			slug='test_slug',
+			description="test description",
+			url='https://www.test_url.duck')
+		user = User.objects.create(
+			id=1,
+			username="Test Username")
+		user.set_password('password')
+		user.save()
+		Comment.objects.create(
+			id=1,
+			text="comment test 1",
+			video=video,
+			user=user)
+		Comment.objects.create(
+			id=2,
+			text='comment test 2',
+			video=video,
+			user=user)
+
+	def test_have_str(self):
+		video = MyVideo.objects.get(id=1)
+		self.assertEqual(video.__str__(), f"{video.title} - {video.slug[:10]}")
+
+	def test_have_comments(self):
+		video = MyVideo.objects.get(id=1)
+		all_comments = Comment.objects.filter(video=video)
+		self.assertCountEqual(video.comment, all_comments)
+
+	def test_long_post(self):
+		video = MyVideo.objects.get(id=1)
+		current_response = date.today() - video.date.date()
+		self.assertEqual(video.long_post, current_response)
+
+	def test_hello_page(self):
+		response = self.client.get('/hello/')
+		self.assertEqual(response.status_code, 200)
+
+	def test_fail_page(self):
+		response = self.client.get('/rtfghf/')
+		self.assertEqual(response.status_code, 404)
+
+	def test_authenticate(self):
+		res = self.client.login(
+			username='Test Username',
+			password='password')
+		self.assertEqual(res, True)
+
+	def test_content(self):
+		self.client.login(
+			username='Test Username',
+			password='password')
+		response = self.client.get('/hello/')
+		self.assertEqual(response.context['user'].username, 'Test Username')
+		#logging.warning(response.context)
+
+	def test_all_video(self):
+		response = self.client.get('/hello/')
+		all_video = MyVideo.objects.all()
+		all_video = [video.to_json() for video in all_video]
+		for video in all_video:
+			del video['long_post']
+		for video in response.context['all_video']:
+			del video['long_post']
+		self.assertEqual(response.context['all_video'], all_video)
+
+
 
 # Create your tests here.
--- src/video/views.py
+++ src/video/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import MyVideo
-
+import logging
 from rest_framework.decorators import api_view
 from django.core.cache import cache
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
